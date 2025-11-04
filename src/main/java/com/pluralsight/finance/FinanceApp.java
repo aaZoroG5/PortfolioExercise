49889a46c7106365d857b1fda7ffef567763b386
andyariza <andyariza90@gmail.com>
2025-11-04 10:30:09 -0500

    tested the given code from the workbook in the finance app

---
+++ src/main/java/com/pluralsight/finance/FinanceApp.java
@@ -0,0 +1,14 @@
+package com.pluralsight.finance;
+
+public class FinanceApp {
+
+    public static void main(String[] args) {
+
+        BankAccount account1 = new BankAccount("Pam", "123", 12500);
+        Valuable account2 = new BankAccount("Gary", "300", 1500);
+
+        account1.deposit(100);
+        account2.deposit(100);
+
+    }
+}
