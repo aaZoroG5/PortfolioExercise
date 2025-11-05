2cfea16fb2c13454c044b175394e38e5f0ef4d48
Andy <test@email.com>
2025-11-05 02:36:50 -0500

    added notes for the difference between an abstract class and an interface

--- src/main/java/com/pluralsight/finance/Valuable.java
+++ src/main/java/com/pluralsight/finance/Valuable.java
@@ -3,4 +3,8 @@ package com.pluralsight.finance;
 public interface Valuable {
     //this method needs to be overridden to any class it gets implemented into
     public double getValue();
+    //NOTE: the difference between an abstract class and an interface is that an abstract can only be inherited once
+    //by a child class, meanwhile, more than one interface can be implemented for a single class
+    //Ex: public class Dog extends Animal <== the parent class, Animal, is abstract
+    //    public class Cat implements Pet, Hunter <== the Cat class implements methods from both interfaces, Pet and Hunter
 }
